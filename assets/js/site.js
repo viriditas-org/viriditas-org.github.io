fdef82bbc233e17ef41266dc4498a8e6aeb4ceae
Misha Wolfson <misha@ginkgobioworks.com>
2018-09-05 15:38:08 -0400

    Collapse nav menu on click

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,20 +5,27 @@ $(function () {
   /** Initialize scrollspy */
   $('body').scrollspy({ target: '#navitems', offset: navHeight })
 
-
   /**
    * Smooth scroll for nav item links
    */
   $('.scroll-smooth').on('click', function (event) {
-    var hash = this.hash; // must store as variable for lexical scope
+    var target = this;
+    var hash = target.hash; // must store as variable for lexical scope
     var scrollTime = 500;
     var scrollTop;
 
     // Only do smooth scrolling if we're not changing the path and are directed toward a hash
-    if (hash && this.pathname === window.location.pathname) {
+    if (hash && target.pathname === window.location.pathname) {
+      // Cancel the normal click actions to prevent misalignment to the top
       event.preventDefault();
-      scrollTop = $(hash).offset().top - navHeight;
 
+      // Hide any parent collapsables (default click behavior)
+      $(target)
+        .parents('.collapse')
+        .collapse('hide');
+
+      // Slowly scroll to the appropriate position
+      scrollTop = $(hash).offset().top - navHeight;
       $('html, body')
         .animate({ scrollTop: scrollTop }, scrollTime)
         .promise()
